968eb0f39e6df013f51b2b46111b836e07d992a7
Kate Tran <katr1900@student.miun.se>
2021-02-24 22:31:06 +0100

    Initial

---
+++ Moment32/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moment32.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Moment32.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Song> Songs { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Song>().HasKey(a => a.Id);
+            modelBuilder.Entity<Song>().Property(a => a.Id).ValueGeneratedOnAdd();
+
+        }
+    }
+}
---
+++ Moment32/Models/Song.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Moment32.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public int Length { get; set; }
+        public string Category { get; set; }
+    }
+}
